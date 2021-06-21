5edfa470b0fbc3492226c359b9919a6a2f064e37
Ibrahim <ibrahim7101999@gmail.com>
2021-06-21 05:05:22 +0200

    No edit yet

---
+++ src/first/First.java
@@ -0,0 +1,13 @@
+
+package first;
+
+public class First {
+    
+    public static void main(String[] args) {
+        System.out.println("السلام عليكم");
+        System.out.println("مع أول مشروع");
+        System.out.println("في الجافا");
+
+    }
+    
+}
